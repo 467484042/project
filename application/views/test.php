0bd9ff42ceb55026adccffcacef0edce60931b3e
haoranpan <haoranpan3612089@gmail.com>
2020-10-15 21:50:10 +1000

    1
    1

---
+++ application/views/test.php
@@ -0,0 +1,15 @@
+<html lang="en">
+<head>
+
+</head>
+<body>
+
+<div id="container">
+<?php echo "base url: ".base_url();?>
+<?php echo "<br>";?>
+<?php echo "site url: ".site_url();?>
+<?php echo "<br>";?>
+<?php echo "SERVER['SERVER_NAME']: ".$_SERVER['SERVER_NAME'];?>
+</div>
+
+</body>
